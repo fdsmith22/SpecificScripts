55bbe4920bd69b1da3ae7660f2f86718f8844fb9
fdsmith22 <fdsmith.mail@gmail.com>
2025-03-20 18:13:49 +0000

    Update power_monitor.py

--- power_monitor.py
+++ power_monitor.py
@@ -1,15 +1,4 @@
 
-# Use this script to create a log for battery testing,
-# Run the script with <sudo python3 power_monitor.py> to write the log file to the home/dir
-
-
-# The log file will auto rewrite the previous log each time it's ran -- so save it or lose it!
-
-# Feel free to modify :) 
-
-
-
-
 
 
 
